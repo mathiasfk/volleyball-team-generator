533d90347ca6ca60895d750feaa45e945a16bf3c
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-10-22 08:07:19 -0300

    test: improve test organization

--- src/lib/calculateTeams.test.ts
+++ src/lib/calculateTeams.test.ts
@@ -3,51 +3,59 @@ import { calculateTeams } from './calculateTeams'
 
 describe('calculateTeams', () => {
 
-  test.for([
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
-  ])('should create two balanced teams > %i participants', (numberOfParticipants) => {
-    const participants = generateParticipants(numberOfParticipants)
-    const { formedTeams } = calculateTeams(participants)
-    expect(formedTeams).toHaveLength(2)
-    expect(formedTeams[0].length).toBeCloseTo(formedTeams[1].length, 1) // Teams should be balanced
-    expect(formedTeams[0].length).toBe(formedTeams[1].length) // Teams should be balanced
+  describe('should create two balanced teams', () => {
+    test.for([
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
+    ])('%i participants', (numberOfParticipants) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const { formedTeams } = calculateTeams(participants)
+      expect(formedTeams).toHaveLength(2)
+      expect(formedTeams[0].length).toBeCloseTo(formedTeams[1].length, 1) // Teams should be balanced
+      expect(formedTeams[0].length).toBe(formedTeams[1].length) // Teams should be balanced
+    })
   })
 
-  test.for([
-    [1, 1],
-    [0, 2],
-    [1, 3],
-    [0, 4],
-    [1, 7],
-    [0, 12],
-    [1, 13],
-    [5, 17],
-  ])('should leave %i people on the bench > % i participants', ([expectedBenchCount, numberOfParticipants]) => {
-    const participants = generateParticipants(numberOfParticipants)
-    const { remainingPlayers } = calculateTeams(participants)
-    expect(remainingPlayers).toHaveLength(expectedBenchCount)
+  describe('should leave people on the bench', () => {
+    test.for([
+      [1, 1],
+      [0, 2],
+      [1, 3],
+      [0, 4],
+      [1, 7],
+      [0, 12],
+      [1, 13],
+      [5, 17],
+    ])('%i on bench with %i participants', ([expectedBenchCount, numberOfParticipants]) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const { remainingPlayers } = calculateTeams(participants)
+      expect(remainingPlayers).toHaveLength(expectedBenchCount)
+    })
   })
 
-  test.for([
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
-  ])('should be no missing people > %i participants', (numberOfParticipants) => {
-    const participants = generateParticipants(numberOfParticipants)
-    const { formedTeams, remainingPlayers } = calculateTeams(participants)
-    const peoplePlaying = formedTeams.flat().length
-    const peopleOnBench = remainingPlayers.length
-    expect(peoplePlaying + peopleOnBench).toBe(numberOfParticipants)
+  describe('should be no missing people', () => {
+    test.for([
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
+    ])('%i participants', (numberOfParticipants) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const { formedTeams, remainingPlayers } = calculateTeams(participants)
+      const peoplePlaying = formedTeams.flat().length
+      const peopleOnBench = remainingPlayers.length
+      expect(peoplePlaying + peopleOnBench).toBe(numberOfParticipants)
+    })
   })
 
-  test.for([
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
-  ])('should have at most 6 players per team > %i participants', (numberOfParticipants) => {
-    const participants = generateParticipants(numberOfParticipants)
-    const { formedTeams } = calculateTeams(participants)
-    
-    expect(formedTeams[0].length).toBeLessThanOrEqual(6)
-    expect(formedTeams[1].length).toBeLessThanOrEqual(6)
-    expect(formedTeams[0].length).toBeGreaterThanOrEqual(1)
-    expect(formedTeams[1].length).toBeGreaterThanOrEqual(1)
+  describe('should have at most 6 players per team', () => {
+    test.for([
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
+    ])('%i participants', (numberOfParticipants) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const { formedTeams } = calculateTeams(participants)
+      
+      expect(formedTeams[0].length).toBeLessThanOrEqual(6)
+      expect(formedTeams[1].length).toBeLessThanOrEqual(6)
+      expect(formedTeams[0].length).toBeGreaterThanOrEqual(1)
+      expect(formedTeams[1].length).toBeGreaterThanOrEqual(1)
+    })
   })
 })
 
