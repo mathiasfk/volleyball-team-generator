74a455b50186c84414d7027aed04c0f23a239c4f
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-10-23 07:27:24 -0300

    test: add more test scenarios

--- src/lib/calculateTeams.test.ts
+++ src/lib/calculateTeams.test.ts
@@ -14,6 +14,23 @@ describe('calculateTeams', () => {
     })
   })
 
+  describe('should create two balanced teams on new match', () => {
+    test.for([
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
+    ])('%i participants', (numberOfParticipants) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const prevMatch = calculateTeams({participants})
+      const { formedTeams } = calculateTeams({
+        participants,
+        teams: prevMatch.formedTeams,
+        benchPlayers: prevMatch.remainingPlayers,
+        keepTeamId: [null, 0, 1][Math.floor(Math.random()*2)] // randomly keep one team intact
+      })
+      expect(formedTeams).toHaveLength(2)
+      expect(formedTeams[0].length).toBe(formedTeams[1].length) // Teams should be balanced
+    })
+  })
+
   describe('should leave people on the bench', () => {
     test.for([
       [1, 1],
@@ -30,6 +47,49 @@ describe('calculateTeams', () => {
       const participants = generateParticipants(numberOfParticipants)
       const { remainingPlayers } = calculateTeams({participants})
       expect(remainingPlayers).toHaveLength(expectedBenchCount)
+    })
+  })
+
+  describe('should leave people on the bench on new match', () => {
+    test.for([
+      [1, 1],
+      [0, 2],
+      [1, 3],
+      [0, 4],
+      [0, 6],
+      [1, 7],
+      [0, 8],
+      [0, 12],
+      [1, 13],
+      [5, 17],
+    ])('%i on bench with %i participants', ([expectedBenchCount, numberOfParticipants]) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const prevMatch = calculateTeams({participants})
+      const { remainingPlayers } = calculateTeams({
+        participants,
+        teams: prevMatch.formedTeams,
+        benchPlayers: prevMatch.remainingPlayers,
+        keepTeamId: [null, 0, 1][Math.floor(Math.random()*2)] // randomly keep one team intact
+      })
+      expect(remainingPlayers).toHaveLength(expectedBenchCount)
+    })
+  })
+
+  describe('should be no missing people on new match', () => {
+    test.for([
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 36
+    ])('%i participants', (numberOfParticipants) => {
+      const participants = generateParticipants(numberOfParticipants)
+      const prevMatch = calculateTeams({participants})
+      const { formedTeams, remainingPlayers } = calculateTeams({
+        participants,
+        teams: prevMatch.formedTeams,
+        benchPlayers: prevMatch.remainingPlayers,
+        keepTeamId: [null, 0, 1][Math.floor(Math.random()*2)] // randomly keep one team intact
+      })
+      const peoplePlaying = formedTeams.flat().length
+      const peopleOnBench = remainingPlayers.length
+      expect(peoplePlaying + peopleOnBench).toBe(numberOfParticipants)
     })
   })
 
